5a4db7f42fc3b6eff19bb7ce23a67d100a86a8dc
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-09-13 19:08:19 +0530

    tabBar modified

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { useAuth } from '../../provider/AuthContext'
 import { signOut } from 'firebase/auth'
 import { FIREBASE_AUTH } from '../../config/firebaseConfig'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const TabLayout = () => {
   const { user } = useAuth();
@@ -20,30 +21,41 @@ const TabLayout = () => {
   }
 
   return (
-    <Tabs screenOptions={{ headerTitleAlign: 'center' }}>
+    <Tabs screenOptions={{
+      headerTitleAlign: 'center',
+      headerRight: () => {
+        return (
+          <Pressable onPress={() => handleLogout()}>
+            <View style={{
+              marginRight:10,
+              flexDirection: 'row',
+              gap: 4,
+              alignItems: 'center',
+            }}>
+              <Text style={{
+                color: "#0ea5e9",
+                fontWeight: '400',
+                fontSize: 14
+              }}>Logout</Text>
+              <MaterialIcons name="logout" size={20} color="#0ea5e9" />
+            </View>
+          </Pressable>
+        )
+      },
+    }}>
+
       <Tabs.Screen name='index' options={{
         title: "Chat Groups",
-        headerRight: () => {
-          return (
-            <Pressable onPress={() => handleLogout()}>
-              <View style={{
-                paddingHorizontal: 15,
-                flexDirection: 'row',
-                gap: 4,
-                alignItems: 'center',
-              }}>
-                <Text style={{
-                  color: "#0ea5e9",
-                  fontWeight: '400',
-                  fontSize: 14
-                }}>Logout</Text>
-                <MaterialIcons name="logout" size={20} color="#0ea5e9" />
-              </View>
-            </Pressable>
-          )
-        }
+        tabBarLabel: "Chats",
+        tabBarIcon: ({ color, size }) => <Ionicons name="chatbubbles" size={size} color={color} />
       }} />
-      <Tabs.Screen name='groups' options={{ title: "Groups" }} />
+
+      <Tabs.Screen name='profile' options={{
+        title: "My Profile",
+        tabBarLabel: "My Profile",
+        tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} />
+      }} />
+
     </Tabs>
   )
 }

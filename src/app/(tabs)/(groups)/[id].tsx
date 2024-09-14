145beafc09b145c1a96910b49b2dd05e9b124a8e
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-09-14 20:12:04 +0530

    send button disable mode added

--- src/app/(tabs)/(groups)/[id].tsx
+++ src/app/(tabs)/(groups)/[id].tsx
@@ -1,16 +1,18 @@
 import { View, Text, StyleSheet, TextInput, Pressable, TouchableOpacity, FlatList, ImageBackground } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { addDoc, collection, DocumentData, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
 import { FIRESTORE_DB } from '../../../config/firebaseConfig';
 import { useAuth } from '../../../provider/AuthContext';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const Chat = () => {
   const { id } = useLocalSearchParams();
   const { user } = useAuth();
   const [messages, setMessages] = useState<DocumentData[]>([]);
   const [message, setMessage] = useState<string>('');
+  const flatListRef = useRef(null);
 
   useEffect(() => {
     const msgCollectionRef = collection(FIRESTORE_DB, `groups/${id}/messages`);
@@ -28,7 +30,7 @@ const Chat = () => {
 
   const sendMessages = async () => {
     const msg = message.trim();
-
+    setMessage("");
     if (msg.length === 0) {
       return;
     }
@@ -39,8 +41,6 @@ const Chat = () => {
       sender: user.uid,
       createdAt: serverTimestamp(),
     })
-
-    setMessage("");
   }
 
   const renderMessages = ({ item }: { item: DocumentData }) => {
@@ -67,24 +67,25 @@ const Chat = () => {
       </View>
     )
   }
-  const image = { uri: 'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.pn' };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require("../../../../assets/background.png")} resizeMode="cover" style={{ flex: 1, padding: 10 }}>
-        <FlatList keyExtractor={(item) => item.id} data={messages} renderItem={renderMessages} contentContainerStyle={{ gap: 10, paddingBottom: 20 }} showsVerticalScrollIndicator={false} />
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={messages}
+          renderItem={renderMessages}
+          contentContainerStyle={{ gap: 10, paddingBottom: 20 }}
+          showsVerticalScrollIndicator={false}
+          ref={flatListRef}
+        />
 
         <View style={styles.inputContainer}>
           <TextInput value={message} placeholder='Message' multiline onChangeText={setMessage} style={styles.textInput} />
 
-          <TouchableOpacity style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: "#0ea5e9",
-            width: 45,
-            height: 45,
-            borderRadius: 25,
-          }} onPress={() => sendMessages()}>
+          <TouchableOpacity style={[styles.sendBtn, {
+            backgroundColor: message.length === 0 ? "#93c5fd" : "#0ea5e9"
+          }]} onPress={() => sendMessages()}>
             <FontAwesome name="send" size={20} color="#fff" />
           </TouchableOpacity>
         </View>
@@ -112,5 +113,13 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingVertical: 8,
     paddingHorizontal: 14,
+  },
+  sendBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 45,
+    height: 45,
+    borderRadius: 25,
   }
 })
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../../provider/AuthContext'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { FIRESTORE_DB } from '../../config/firebaseConfig';
 
 const Groups = () => {
+  const { user } = useAuth();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    getUserProfileData(user.uid);
+  }, [])
+
+  const getUserProfileData = async (docId: string) => {
+    try {
+      const docRef = doc(FIRESTORE_DB, 'users', docId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setUsername(docSnap.data()?.username)
+        setEmail(docSnap.data()?.email)
+        return docSnap.data();
+      } else {
+        console.log('No such document!');
+      }
+    } catch (error) {
+      console.error('Error getting document:', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>two</Text>
+    <View style={{ padding: 10 }}>
+      <Text style={{ fontSize: 18 }}>{username}</Text>
+      <Text style={{ fontSize: 18 }}>{email}</Text>
     </View>
   )
 }
